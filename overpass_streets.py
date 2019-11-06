bf0daa8b6a782fd59f2c9a583e37e78481392955
Nikolay Martyanov <nikolay.martyanov@opensynergy.com>
2019-11-06 20:36:42 +0100

    Add debug and verbose modes.
    In the debug mode the script will handle only one way.
    In the verbose mode the script will show all metadata about processed
    points.
    
    Using the `argrpase` module, add `--debug` and `--verbose` flags.

--- overpass_streets.py
+++ overpass_streets.py
@@ -4,14 +4,23 @@ from google_key import KEY
 import requests
 import os
 import json
+import argparse
 
-debug = True
+verbose = False
+debug = False
 
 query = """
 area[name="Королёв"];
 (way["highway"](area); >;);
 out skel;
 """
+
+
+def verbose_info(*args, **kwargs):
+    if verbose:
+        print(*args, **kwargs)
+    else:
+        pass
 
 
 def get_geoline_props(lat1, lat2, long1, long2):
@@ -28,6 +37,7 @@ def streetview_available(lat, lon):
         "key": KEY,
     }
     r = requests.get(google_meta_api_url, params=meta_request_params)
+    verbose_info(f"\t\tStreet View meta request: {r.request.url}")
     image_meta = json.loads(r.content)
     if image_meta['status'] != "OK":
         return False
@@ -45,40 +55,50 @@ def grab_streetview(lat, lon, heading, download_dir, filename):
         "heading": f"{heading:f}"
     }
     r = requests.get(google_api_url, params=view_request_params)
+    verbose_info(f"\t\tStreet View request: {r.request.url}")
     full_file = os.path.join(download_dir, filename)
+    verbose_info(f"\t\t\tFile to save: {full_file}")
     open(full_file, 'wb').write(r.content)
 
 
 def main():
+    argparser = argparse.ArgumentParser(description="The script grabs Google Street View for a city.")
+    argparser.add_argument("--debug", action="store_true", help="run in debug mode")
+    argparser.add_argument("--verbose", action="store_true", help="show info about all the processed points")
+    args = argparser.parse_args()
+    global verbose, debug
+    verbose = args.verbose
+    debug = args.debug
+
     api = overpy.Overpass()
     result = api.query(query)
 
     ways = result.ways
     if debug:
-        ways = ways[0:3]
+        ways = ways[0:1]
 
     images_dir = "images"
     if not os.path.isdir(images_dir):
        os.mkdir(images_dir)
 
     for way in ways:
-        print(f'Way {way.id:d}')
+        verbose_info(f"Way {way.id:d}")
         nodes = way.get_nodes()
         for segment in range(0, len(nodes)-1):
-            print("\tSegment " + str(segment))
+            verbose_info(f"\tSegment {segment:d}")
             node_start = nodes[segment]
             node_end = nodes[segment+1]
-            print(f"\t\tstart: {node_start.lat:f}, {node_start.lon:f}")
-            print(f"\t\tend: {node_end.lat:f}, {node_end.lon:f}")
+            verbose_info(f"\t\tstart: {node_start.lat:f}, {node_start.lon:f}")
+            verbose_info(f"\t\tend: {node_end.lat:f}, {node_end.lon:f}")
             azimuth, length = get_geoline_props(node_start.lat, node_end.lat, node_start.lon, node_end.lon)
-            print(f"\t\tazimuth: {azimuth:f}")
-            print(f"\t\tlength: {length:f}")
+            verbose_info(f"\t\tazimuth: {azimuth:f}")
+            verbose_info(f"\t\tlength: {length:f}")
 
             # Count the mid point of the way. Later - switch to a loop for each 10 meters.
             geodesic_mid = Geodesic.WGS84.Direct(node_start.lat, node_start.lon, azimuth, length/2)
             mid_lat = geodesic_mid['lat2']
             mid_lon = geodesic_mid['lon2']
-            print(f"\t\tmid: {mid_lat:f}, {mid_lon:f}")
+            verbose_info(f"\t\tmid: {mid_lat:f}, {mid_lon:f}")
 
             if not streetview_available(mid_lat, mid_lon):
                 continue
